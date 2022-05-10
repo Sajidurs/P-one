6920c9fbeba1b0c82a24a1cdbe7dfaa12eb02362
Sajidur Rahman <shajidur171@gmail.com>
2022-05-10 12:56:23 -0700

    Last

--- functions.php
+++ functions.php
@@ -16,9 +16,8 @@ function one_setup() {
     load_theme_textdomain('one', get_template_directory() . '/languages');
 
     register_nav_menus(array(
-        'primary-menu' => __('Primary Menu', 'halim')
+        'primary-menu' => __('Primary Menu', 'one')
     ));
-	
 			add_theme_support(
 			'html5',
 			array(
@@ -32,14 +31,48 @@ function one_setup() {
 				'navigation-widgets',
 			)
 		);
-		add_editor_style("/assets/css/editor.css");
-		//css blank takbe
+		add_editor_style("/css/editor.css");
 		
-		add_image_size( 'category-thumb', 300, 9999 );
-		the_post_thumbnail('category-thumb');
-		the_post_thumbnail( 'thumbnail', array( 'class' => 'alignleft' ) );
-		the_post_thumbnail( 'thumbnail', array( "left","bottom" ) );
-		//Regenerate thumbnail plugin lagbe
 	
 }
-add_action('after_setup_theme', 'halim_setup');
+add_action('after_setup_theme', 'one_setup');
+
+// Enqueing Files
+function one_assets() {
+    
+    //Calling From Web
+    wp_enqueue_style( 'font-opensans', '//fonts.googleapis.com/css2?family=Open+Sans:wght@400;500&family=Roboto:wght@500;700;900&display=swap', array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'font-domain1', '//fonts.googleapis.com', array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'font-domain2', '//fonts.gstatic.com', array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'fontawesome-1', '//cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css', array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'fontawesome-2', '//cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css', array(), '1.0.0', 'all' );
+
+    // Calling From Local
+    wp_enqueue_style( 'main-css', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '1.0.0', 'all');
+    wp_enqueue_style( 'lib-css', get_template_directory_uri() . '/lib/animate/animate.min.css', array(), '1.0.0', 'all');
+    wp_enqueue_style( 'owlcarousel-css', get_template_directory_uri() . '/lib/owlcarousel/assets/owl.carousel.min.css', array(), '1.0.0', 'all');
+    wp_enqueue_style( 'owlcarousel-css2', get_template_directory_uri() . '/lib/owlcarousel/assets/owl.carousel.css', array(), '1.0.0', 'all');
+    wp_enqueue_style( 'owlcarousel-css3', get_template_directory_uri() . '/lib/owlcarousel/assets/owl.theme.default.css', array(), '1.0.0', 'all');
+    wp_enqueue_style( 'owlcarousel-css4', get_template_directory_uri() . '/lib/owlcarousel/assets/owl.theme.default.min.css', array(), '1.0.0', 'all');
+    wp_enqueue_style( 'owlcarousel-css5', get_template_directory_uri() . '/lib/owlcarousel/assets/owl.theme.green.css', array(), '1.0.0', 'all');
+    wp_enqueue_style( 'owlcarousel-css6', get_template_directory_uri() . '/lib/owlcarousel/assets/owl.theme.green.min.css', array(), '1.0.0', 'all');
+
+    wp_enqueue_style( 'lightbox-css', get_template_directory_uri() . '/lib/lightbox/css/lightbox.min.css', array(), '1.0.0', 'all');
+    wp_enqueue_style( 'style-theme', get_stylesheet_uri() );
+
+    // Calling JS
+    wp_enqueue_script( 'jquery-js', '//code.jquery.com/jquery-3.4.1.min.js' );
+    wp_enqueue_script( 'js-jsdelivr', '//cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js' );
+    wp_enqueue_script( 'js-wow2', get_template_directory_uri() . '/lib/wow/wow.min.js', array('jquery'), '1.0.0', true );
+    wp_enqueue_script( 'js-wow', get_template_directory_uri() . '/lib/wow/wow.js', array('jquery'), '1.0.0', true );
+    wp_enqueue_script( 'js-easing', get_template_directory_uri() . '/lib/easing/easing.min.js', array('jquery'), '1.0.0', true );
+    wp_enqueue_script( 'js-waypoints', get_template_directory_uri() . '/lib/waypoints/waypoints.min.js', array('jquery'), '1.0.0', true );
+    wp_enqueue_script( 'js-counterup', get_template_directory_uri() . '/lib/counterup/counterup.min.js', array('jquery'), '1.0.0', true );
+    wp_enqueue_script( 'js-owlcarousel', get_template_directory_uri() . '/lib/owlcarousel/owl.carousel.min.js', array('jquery'), '1.0.0', true );
+    wp_enqueue_script( 'js-owlcarouse2', get_template_directory_uri() . '/lib/owlcarousel/owl.carousel.js', array('jquery'), '1.0.0', true );
+    wp_enqueue_script( 'js-isotope', get_template_directory_uri() . '/lib/isotope/isotope.pkgd.min.js', array('jquery'), '1.0.0', true );
+    wp_enqueue_script( 'js-lightbox', get_template_directory_uri() . '/lib/lightbox/js/lightbox.min.js', array('jquery'), '1.0.0', true );
+    wp_enqueue_script( 'main-js', get_template_directory_uri() . '/js/main.js', array('jquery'), '1.0.0', true );
+
+}   
+add_action('wp_enqueue_scripts', 'one_assets');
---
+++ header.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset');?>">
+    <title>Solartec - Renewable Energy Website Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="<?php echo get_template_directory_uri();?>/img/favicon.ico" rel="icon">
+
+    <?php wp_head();?>
+</head>
+
+<body <?php body_class();?>>
+    
+
+
+    <!-- Topbar Start -->
+    <div class="container-fluid bg-dark p-0">
+        <div class="row gx-0 d-none d-lg-flex">
+            <div class="col-lg-7 px-5 text-start">
+                <div class="h-100 d-inline-flex align-items-center me-4">
+                    <small class="fa fa-map-marker-alt text-primary me-2"></small>
+                    <small>123 Street, New York, USA</small>
+                </div>
+                <div class="h-100 d-inline-flex align-items-center">
+                    <small class="far fa-clock text-primary me-2"></small>
+                    <small>Mon - Fri : 09.00 AM - 09.00 PM</small>
+                </div>
+            </div>
+            <div class="col-lg-5 px-5 text-end">
+                <div class="h-100 d-inline-flex align-items-center me-4">
+                    <small class="fa fa-phone-alt text-primary me-2"></small>
+                    <small>+012 345 6789</small>
+                </div>
+                <div class="h-100 d-inline-flex align-items-center mx-n2">
+                    <a class="btn btn-square btn-link rounded-0 border-0 border-end border-secondary" href=""><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-square btn-link rounded-0 border-0 border-end border-secondary" href=""><i class="fab fa-twitter"></i></a>
+                    <a class="btn btn-square btn-link rounded-0 border-0 border-end border-secondary" href=""><i class="fab fa-linkedin-in"></i></a>
+                    <a class="btn btn-square btn-link rounded-0" href=""><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Topbar End -->
+
+
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
+        <a href="index.html" class="navbar-brand d-flex align-items-center border-end px-4 px-lg-5">
+            <h2 class="m-0 text-primary">Solartec</h2>
+        </a>
+        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto p-4 p-lg-0">
+                <a href="index.html" class="nav-item nav-link active">Home</a>
+                <a href="about.html" class="nav-item nav-link">About</a>
+                <a href="service.html" class="nav-item nav-link">Service</a>
+                <a href="project.html" class="nav-item nav-link">Project</a>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                    <div class="dropdown-menu bg-light m-0">
+                        <a href="feature.html" class="dropdown-item">Feature</a>
+                        <a href="quote.html" class="dropdown-item">Free Quote</a>
+                        <a href="team.html" class="dropdown-item">Our Team</a>
+                        <a href="testimonial.html" class="dropdown-item">Testimonial</a>
+                        <a href="404.html" class="dropdown-item">404 Page</a>
+                    </div>
+                </div>
+                <a href="contact.html" class="nav-item nav-link">Contact</a>
+            </div>
+            <a href="" class="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block">Get A Quote<i class="fa fa-arrow-right ms-3"></i></a>
+        </div>
+    </nav>
+    <!-- Navbar End -->
+
+
+    <!-- Carousel Start 
+    <div class="container-fluid p-0 pb-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="owl-carousel header-carousel position-relative">
+            <div class="owl-carousel-item position-relative" data-dot="<img src='<?php // echo get_template_directory_uri();?>/img/carousel-1.jpg'>">
+                <img class="img-fluid" src="<?php // echo get_template_directory_uri();?>/img/carousel-1.jpg" alt="">
+                <div class="owl-carousel-inner">
+                    <div class="container">
+                        <div class="row justify-content-start">
+                            <div class="col-10 col-lg-8">
+                                <h1 class="display-2 text-white animated slideInDown">Pioneers Of Solar And Renewable Energy</h1>
+                                <p class="fs-5 fw-medium text-white mb-4 pb-3">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
+                                <a href="" class="btn btn-primary rounded-pill py-3 px-5 animated slideInLeft">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="owl-carousel-item position-relative" data-dot="<img src='<?php // echo get_template_directory_uri();?>/img/carousel-2.jpg'>">
+                <img class="img-fluid" src="<?php // echo get_template_directory_uri();?>/img/carousel-2.jpg" alt="">
+                <div class="owl-carousel-inner">
+                    <div class="container">
+                        <div class="row justify-content-start">
+                            <div class="col-10 col-lg-8">
+                                <h1 class="display-2 text-white animated slideInDown">Pioneers Of Solar And Renewable Energy</h1>
+                                <p class="fs-5 fw-medium text-white mb-4 pb-3">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
+                                <a href="" class="btn btn-primary rounded-pill py-3 px-5 animated slideInLeft">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="owl-carousel-item position-relative" data-dot="<img src='<?php // echo get_template_directory_uri();?>/img/carousel-3.jpg'>">
+                <img class="img-fluid" src="<?php // echo get_template_directory_uri();?>/img/carousel-3.jpg" alt="">
+                <div class="owl-carousel-inner">
+                    <div class="container">
+                        <div class="row justify-content-start">
+                            <div class="col-10 col-lg-8">
+                                <h1 class="display-2 text-white animated slideInDown">Pioneers Of Solar And Renewable Energy</h1>
+                                <p class="fs-5 fw-medium text-white mb-4 pb-3">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
+                                <a href="" class="btn btn-primary rounded-pill py-3 px-5 animated slideInLeft">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+   Carousel End -->
